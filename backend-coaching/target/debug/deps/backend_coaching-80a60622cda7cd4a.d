0f6ae24217ab33c635b4e3016938d16fe3d7e4d1
Facundo Alejandro Caniza <facundocaniza@proton.me>
2026-01-12 09:41:56 -0300

    first commit

--- backend-coaching/target/debug/deps/backend_coaching-80a60622cda7cd4a.d
+++
@@ -1,5 +0,0 @@
-/home/canizafa/Documentos/proyectos-propios/coaching/backend-coaching/target/debug/deps/backend_coaching-80a60622cda7cd4a.d: src/main.rs
-
-/home/canizafa/Documentos/proyectos-propios/coaching/backend-coaching/target/debug/deps/libbackend_coaching-80a60622cda7cd4a.rmeta: src/main.rs
-
-src/main.rs:
